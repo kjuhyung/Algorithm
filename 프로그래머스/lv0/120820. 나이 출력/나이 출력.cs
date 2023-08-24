ded976ed71d508a3d502a0c30964b576f423072b
kim ju hyung <141553708+kjuhyung@users.noreply.github.com>
2023-08-24 11:09:38 +0900

    [level 0] Title: 나이 출력, Time: 0.15 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int age) 
+    {
+        int answer = 0;
+        if (0 < age && age <= 120)
+        {
+            answer = 2022 - age + 1;
+        }
+        return answer;
+    }
+}
