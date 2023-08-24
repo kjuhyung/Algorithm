d039f4deb0c41815d88c013c217a4350146e1b0b
kim ju hyung <141553708+kjuhyung@users.noreply.github.com>
2023-08-24 11:10:42 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.14 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) 
+    {
+      
+        if (num1 == num2)
+        {
+            return 1;
+        }
+        else
+        {
+            return -1;
+        }
+    
+    }
+}
