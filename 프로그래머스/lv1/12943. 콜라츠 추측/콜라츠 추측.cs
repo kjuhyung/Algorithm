4860b6711eaf5e89621046d65dfbfa93ba2c306a
kim ju hyung <141553708+kjuhyung@users.noreply.github.com>
2023-08-30 10:58:30 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.28 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.cs
@@ -0,0 +1,23 @@
+public class Solution
+{
+    public long solution(long num) 
+    {
+        long answer = 0;
+        int i = 0;
+        for (i = 1; num != 1; i++)
+        {
+            if (num%2 == 0)
+            {
+                num /= 2;
+            }
+            else if (num % 2 !=0)
+            {
+                num = num * 3+1;
+            }
+            if (i >= 500)
+                return -1; 
+        }
+        answer = i - 1;
+        return answer;
+    }
+}
