9f2c6970a9204338324ba669f39647da3eea1956
kim ju hyung <141553708+kjuhyung@users.noreply.github.com>
2023-09-01 18:36:40 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.20 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
@@ -0,0 +1,12 @@
+public class Solution
+{
+    public string solution(int n)
+    {
+        string answer = "";
+        for (int i = 0; i<n; i++)
+        {
+            answer += i%2==0 ? "수" : "박";
+        }
+        return answer;
+    }
+}
