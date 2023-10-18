04d5d5bc855e3f1a4dd53c50e2dda7525c5f9685
kim ju hyung <141553708+kjuhyung@users.noreply.github.com>
2023-10-18 09:27:34 +0900

    [level 1] Title: 콜라 문제, Time: 0.17 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int a, int b, int n) 
+    {
+        int answer = 0;
+        
+        while ( n >= a)
+        {
+            answer += (n/a) * b;
+            n = ((n/a) * b) + (n % a);
+        }
+        
+        return answer;
+    }
+}
